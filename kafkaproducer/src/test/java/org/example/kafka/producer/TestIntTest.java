0aaf1c9b3e27e44b3682086613048ec236ed4bdd
Kevin King <jinzuojiao1981@gmail.com>
2021-08-14 23:14:13 +0800

    add unit test for producer

---
+++ kafkaproducer/src/test/java/org/example/kafka/producer/TestIntTest.java
@@ -0,0 +1,13 @@
+package org.example.kafka.producer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestIntTest {
+
+    @Test
+    void helloSuccess() {
+        assertTrue(true);
+    }
+}
