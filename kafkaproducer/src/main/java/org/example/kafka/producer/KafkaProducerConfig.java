d5d11b4a3f2ea7ab3f7b9bcc3bc3df56a118f481
Kevin King <jinzuojiao1981@gmail.com>
2021-08-14 16:01:54 +0800

    fix error

--- kafkaproducer/src/main/java/org/example/kafka/producer/KafkaProducerConfig.java
+++ kafkaproducer/src/main/java/org/example/kafka/producer/KafkaProducerConfig.java
@@ -18,7 +18,7 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
-    @Bean2
+    @Bean
     public ProducerFactory<String, KafkaMessage> producerFactory() {
         return new DefaultKafkaProducerFactory<>(producerConfigs());
     }
